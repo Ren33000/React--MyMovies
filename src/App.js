bb2a307582378371883aa11139bad51cc9a35681
Mylene <pont.mylene@gmail.com>
2021-12-22 16:05:37 +0900

    favourite list created, add movies to favourite list function ok

--- src/App.js
+++ src/App.js
@@ -4,33 +4,62 @@ import './App.css';
 import MovieList from './components/MovieList';
 import Header from './components/Header';
 import SearchBar from './components/SearchBar';
+import Favourite from './components/Favourite'
+import RemoveFav from './components/RemoveFav';
+
 
 const App = () => {
   const [movies, setMovies] = useState([]);
+  const [favourites, setFavourites] = useState([])
   const [userQuery, setUserQuery] = useState ('');
 
+  // API Fetch
   const getMovieRequest = async (userQuery) => {
     const url = `https://api.themoviedb.org/3/search/movie?api_key=0b6254970863d027fde0a82757987a56&query=${userQuery}`   
     const response = await fetch(url);
     const responseJson = await response.json();
-    // console.log(responseJson)
     if(responseJson.results) {
       setMovies(responseJson.results);
     }
   }
 
+  // Get Query from user in Searchbar
   useEffect (() =>{
     getMovieRequest(userQuery);
   }, [userQuery]);
 
+  // Create the list of favourite movies
+  const addFavouriteMovies = (movie) => {
+    const newFavouriteList = [...favourites, movie];
+    setFavourites(newFavouriteList);
+  }
+
   return (
     <div className='container-fluid movie-app'>
+      {/* Movie List Header + SearchBar + List display */}
       <div className='row d-flex align-items-center mt-4 mb-4'>
         <Header heading="Movies"/>
         <SearchBar userQuery={userQuery} setUserQuery={setUserQuery}/>
       </div>
+
       <div className='row'>
-        <MovieList movies={movies} key={movies.uniqueId}/>
+        <MovieList 
+          movies={movies} 
+          handleFavouritesClick={addFavouriteMovies} 
+          key={movies.uniqueId} 
+          favourite={Favourite}/>
+      </div> 
+
+      {/* Favourite List Header + List display */}
+      <div className='row d-flex align-items-center mt-4 mb-4'>
+        <Header heading="Favourites"/>
+      </div>
+      <div className='row'>
+        <MovieList 
+          movies={favourites} 
+          handleFavouritesClick={addFavouriteMovies} 
+          key={movies.uniqueId} 
+          favourite={Favourite}/>
       </div> 
     </div>
   );
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,12 +1,17 @@
 import React from 'react';
 
 const MovieList = (props) => { 
+    const FavouriteComponent = props.favourite;
     
     return (
         <div className='d-flex'>
             {props.movies.map((movie, index) => 
-            <div className='d-flex justify-content-start m-3'>
+            <div className='image-container d-flex justify-content-start m-3'>
                 <img src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`} alt={movie.title} key={movie.uniqueId}/>
+                <div onClick={() => props.handleFavouritesClick(movie)} className='overlay d-flex align-items-center justify-center'>
+                    <FavouriteComponent />
+                </div>
+                
             </div>
             )}
         </div>
